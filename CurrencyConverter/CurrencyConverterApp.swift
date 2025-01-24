a8c8b601cea6c00181b4a0d33528c9080e8d2889
Weerawut <weerawut@live.com>
2025-01-24 11:09:57 +0700

    Initial Commit

---
+++ CurrencyConverter/CurrencyConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencyConverterApp.swift
+//  CurrencyConverter
+//
+//  Created by Weerawut Chaiyasomboon on 24/1/2568 BE.
+//
+
+import SwiftUI
+
+@main
+struct CurrencyConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
